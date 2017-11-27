01863c5dc111b1c0813cb799b8e0e70324bc4f9b
Matthew Seiler <public@flyinghyrax.net>
2017-11-27 11:10:33 -0500

    Major refactoring of model and messages
    View still a mess

--- diffcalc.elm
+++ diffcalc.elm
@@ -2,12 +2,13 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick, onCheck)
 import Array exposing (..)
+import Guards exposing (..)
 
 main : Program Never Model Msg
 main =
     Html.beginnerProgram { model = model, view = view, update = update }
 
--- model
+-- MODEL
 
 type alias Player =
     { name : String
@@ -20,68 +21,81 @@ type alias Enemy =
     , count : Int
     }
 
+type alias InputTableModel a =
+    { rows : Array a
+    , next : a
+    }
+
 type alias Model =
-    { players : Array Player
-    , nextPlayer : Player
-    , enemies : Array Enemy
-    , nextEnemy : Enemy
+    { players : InputTableModel Player
+    , enemies : InputTableModel Enemy
     }
 
 model : Model
 model =
-    { players = Array.empty
-    , nextPlayer = Player "" 1 True
-    , enemies = Array.empty
-    , nextEnemy = Enemy 0 0
+    { players =
+        { rows = Array.empty
+        , next = Player "" 1 True
+        }
+    , enemies =
+        { rows = Array.empty
+        , next = Enemy 0 0
+        }
     }
 
-removePlayer : Int -> Array a -> Array a
-removePlayer idx arr =
-    Array.append (Array.slice 0 idx arr) (Array.slice (idx + 1) (Array.length arr) arr)
 
+-- MESSAGES
+{-
+  These messages make the update function nice, but they are messy to create!
+  It made the view functions ugly. Maybe create some convenience functions
+  for making instances of these messages?
+-}
 
--- messages
--- TODO: There is symmetry here; these can be consolidated if you use different types
+type InputMsg a
+    = Add a
+    | Remove Int
+    | Replace Int a
+    | UpdateNext a
+
 type Msg
-    = AddPlayer Player
-    | RemovePlayer Int
-    | ChangePlayer Int String Int Bool
-    | UpdateNext String Int
-    | AddEnemy Enemy
-    | ChangeEnemy Int Int Int
-    | RemoveEnemy Int
-    | UpdateNextEnemy Int Int
+    = PlayerInput (InputMsg Player)
+    | EnemyInput (InputMsg Enemy)
+
+-- UPDATE
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        AddPlayer newPlayer ->
-            { model | players = Array.push newPlayer model.players }
-        RemovePlayer idx ->
-            { model | players = removePlayer idx model.players }
-        ChangePlayer idx name lvl active ->
-            { model | players = Array.set idx (Player name lvl active) model.players }
-        UpdateNext name lvl ->
-            { model | nextPlayer = Player name lvl True }
-        AddEnemy newEnemy ->
-            { model | enemies = Array.push newEnemy model.enemies }
-        RemoveEnemy idx ->
-            { model | enemies = removePlayer idx model.enemies }
-        ChangeEnemy idx xp count ->
-            { model | enemies = Array.set idx (Enemy xp count) model.enemies }
-        UpdateNextEnemy xp count ->
-            { model | nextEnemy = Enemy xp count }
+        PlayerInput playerMsg ->
+            { model | players = updateInputTable model.players playerMsg }
+        EnemyInput enemyMsg ->
+            { model | enemies = updateInputTable model.enemies enemyMsg }
 
+updateInputTable : InputTableModel a -> InputMsg a -> InputTableModel a
+updateInputTable table msg =
+    case msg of
+        Add it ->
+            { table | rows = Array.push it table.rows }
+        Remove idx ->
+            { table | rows = removeElement idx table.rows }
+        Replace idx it ->
+            { table | rows = Array.set idx it table.rows }
+        UpdateNext it ->
+            { table | next = it }
+
+removeElement : Int -> Array a -> Array a
+removeElement idx arr =
+    Array.append (Array.slice 0 idx arr) (Array.slice (idx + 1) (Array.length arr) arr)
 
 -- VIEW
 view : Model -> Html Msg
 view model =
     div [ id "wrap-entry-tables" ]
-        [ div [] [ characterView model ]
-        , div [] [ enemyView model ]
+        [ div [] [ characterView model.players ]
+        , div [] [ enemyView model.enemies ]
         ]
 
-characterView : Model -> Html Msg
+characterView : InputTableModel Player -> Html Msg
 characterView model =
     table [ class "entry-table" ]
         [ caption [] [ text "Characters" ]
@@ -93,23 +107,25 @@ characterView model =
                 , th [] []
                 ]
             ]
-        , tbody [] (Array.toList (Array.indexedMap makeCharacterRow model.players))
+        , tbody [] (Array.toList (Array.indexedMap makeCharacterRow model.rows))
         , tfoot [] [ tr []
                         [ td [] []
                         , td [] [ input [ type_ "text"
                                         , placeholder "character"
-                                        , value model.nextPlayer.name
-                                        , onInput (\ newName -> UpdateNext newName model.nextPlayer.level)
+                                        , value model.next.name
+                                        , onInput (\ newName -> PlayerInput <| UpdateNext <| Player newName model.next.level True)
                                         ] []
                                 ]
                         , td [] [ input [ type_ "number"
-                                        , value (toString model.nextPlayer.level)
-                                        , onInput (\ newLvlStr -> UpdateNext model.nextPlayer.name (Result.withDefault 0 (String.toInt newLvlStr)))
+                                        , value (toString model.next.level)
+                                        , onInput (\ newLvlStr -> PlayerInput <| UpdateNext <| Player model.next.name
+                                                                                                      (Result.withDefault 0 <| String.toInt newLvlStr)
+                                                                                                      True)
                                         ] []
                                 ]
                         , td [] [ input [ type_ "button"
                                         , value "+"
-                                        , onClick (AddPlayer model.nextPlayer)
+                                        , onClick <| PlayerInput <| Add model.next
                                         ] []
                                 ]
                         ]
@@ -119,35 +135,43 @@ characterView model =
 makeCharacterRow : Int -> Player -> Html Msg
 makeCharacterRow index player =
     tr [ style [("opacity", if player.active then "1.0" else "0.5")] ]
-        [ td    [ onClick (ChangePlayer index player.name player.level (not player.active))
+        [ td    [ onClick (PlayerInput <| Replace index <| Player player.name player.level <| not player.active)
                 ]
                 [ input [ type_ "checkbox"
                         , checked player.active
-                        , onCheck (ChangePlayer index player.name player.level)
+                        , onCheck (\ checked -> PlayerInput <| Replace index <| Player player.name
+                                                                                      player.level
+                                                                                      checked)
                         ]
                         []
                 ]
         , td [] [ input [ type_ "text"
                         , value player.name
-                        , onInput (\ newName -> ChangePlayer index newName player.level player.active )
+                        , onInput (\ newName -> PlayerInput <| Replace index <| Player newName
+                                                                                       player.level
+                                                                                       player.active)
                         ]
                         []
                 ]
         , td [] [ input [ type_ "number"
+                        , Html.Attributes.min "1"
+                        , Html.Attributes.max "20"
                         , value (toString player.level)
-                        , onInput (\ newLvl -> ChangePlayer index player.name (Result.withDefault 0 (String.toInt newLvl)) player.active )
+                        , onInput (\ newLvl -> PlayerInput <| Replace index <| Player player.name
+                                                                                      (Result.withDefault 0 (String.toInt newLvl))
+                                                                                      player.active)
                         ]
                         []
                 ]
         , td [] [ input [ type_ "button"
                         , value "-"
-                        , onClick (RemovePlayer index)
+                        , onClick (PlayerInput <| Remove index)
                         ]
                         []
                 ]
         ]
 
-enemyView : Model -> Html Msg
+enemyView : InputTableModel Enemy -> Html Msg
 enemyView model =
     table [ class "entry-table" ]
             [ caption [] [ text "Enemies" ]
@@ -158,22 +182,22 @@ enemyView model =
                             , th [] []
                             ]
                         ]
-             , tbody [] (Array.toList (Array.indexedMap makeEnemyRow model.enemies))
+             , tbody [] (Array.toList (Array.indexedMap makeEnemyRow model.rows))
              , tfoot [] [ tr [] [ td [] []
                                 , td [] [ input [ type_ "number"
                                                 , placeholder "XP"
-                                                , step "10"
-                                                , onInput (\newXp -> UpdateNextEnemy (Result.withDefault 0 (String.toInt newXp)) model.nextEnemy.count)
+                                                , step "25"
+                                                , onInput (\newXp -> EnemyInput <| UpdateNext <| Enemy (Result.withDefault 0 (String.toInt newXp)) model.next.count)
                                                 ] []
                                         ]
                                 , td [] [ input [ type_ "number"
                                                 , placeholder "Count"
-                                                , onInput (\newCount -> UpdateNextEnemy model.nextEnemy.xp (Result.withDefault 0 (String.toInt newCount)))
+                                                , onInput (\newCount -> EnemyInput <| UpdateNext <| Enemy model.next.xp (Result.withDefault 0 (String.toInt newCount)))
                                                 ] []
                                         ]
                                 , td [] [ input [ type_ "button"
                                                 , value "+"
-                                                , onClick (AddEnemy model.nextEnemy)
+                                                , onClick (EnemyInput <| Add model.next)
                                                 ] []
                                         ]
                                 ] ]
@@ -185,18 +209,105 @@ makeEnemyRow idx enemy =
           , td [] [ input [ type_ "number"
                           , value (toString enemy.xp)
                           , step "10"
-                          , onInput (\ newXp -> ChangeEnemy idx (Result.withDefault 0 (String.toInt newXp)) enemy.count)
+                          , onInput (\ newXp -> EnemyInput <| Replace idx <| Enemy (Result.withDefault 0 (String.toInt newXp)) enemy.count)
                           ] []
                   ]
           , td [] [ input [ type_ "number"
                           , value (toString enemy.count)
-                          , onInput (\ newCount -> ChangeEnemy idx enemy.xp (Result.withDefault 0 (String.toInt newCount)))
+                          , onInput (\ newCount -> EnemyInput <| Replace idx <| Enemy enemy.xp (Result.withDefault 0 (String.toInt newCount)))
                           ] []
                   ]
           , td [] [ input [ type_ "button"
                           , value "-"
-                          , onClick (RemoveEnemy idx)
+                          , onClick (EnemyInput <| Remove idx)
                           ] []
                   ]
           ]
 
+
+-- how to model thresholds for character levels?
+-- how to model multiplier for monster count?
+
+multForEnemyCount : Int -> Float
+multForEnemyCount count = (count <= 1)  => 1.0
+                       |= (count <= 2)  => 1.5
+                       |= (count <= 6)  => 2.0
+                       |= (count <= 10) => 2.5
+                       |= (count <= 14) => 3.0
+                       |= 4.0
+
+type Difficulty
+    = Easy
+    | Medium
+    | Hard
+    | Deadly
+
+type alias Thresholds =
+    { easy : Int
+    , medium : Int
+    , hard : Int
+    , deadly : Int
+    }
+
+getThresholdForDifficulty : Thresholds -> Difficulty -> Int
+getThresholdForDifficulty th diff =
+    case diff of
+        Easy ->
+            th.easy
+        Medium ->
+            th.medium
+        Hard ->
+            th.hard
+        Deadly ->
+            th.deadly
+
+thresholdsForLevels : Array Thresholds
+thresholdsForLevels = Array.fromList
+    [ Thresholds 0    0    0    0
+    , Thresholds 25   50   75   100
+    , Thresholds 50   100  150  200
+    , Thresholds 75   150  225  400
+    , Thresholds 125  250  375  500
+    , Thresholds 250  500  750  1100
+    , Thresholds 300  600  900  1400
+    , Thresholds 350  750  1100 1700
+    , Thresholds 450  900  1400 2100
+    , Thresholds 550  1100 1600 2400
+    , Thresholds 600  1300 1900 2800
+    , Thresholds 800  1600 2400 3600
+    , Thresholds 1000 2000 3000 4500
+    , Thresholds 1100 2200 3400 5100
+    , Thresholds 1250 2500 3800 5700
+    , Thresholds 1400 2800 4300 6400
+    , Thresholds 1600 3200 4800 7200
+    , Thresholds 2000 3900 5900 8800
+    , Thresholds 2100 4200 6300 9500
+    , Thresholds 2400 4900 7300 10900
+    , Thresholds 2800 5700 8500 12700
+    ]
+
+getThresholdsForLevel : Int -> Thresholds
+getThresholdsForLevel lvl =
+    let
+        top = Array.length thresholdsForLevels
+        clampedLvl = clamp 0 top lvl
+    in
+        case Array.get clampedLvl thresholdsForLevels of
+            Just thresh -> thresh
+            _ -> Thresholds 0 0 0 0-- should neve happen since we clamped the level
+
+type alias EnemyXpCalc =
+    { enemy : Enemy
+    , totalXp : Int
+    , mult : Float
+    , adjust : Int
+    }
+
+calcEnemyStuff : Enemy -> EnemyXpCalc
+calcEnemyStuff enemy =
+    let
+        totalXp = enemy.count * enemy.xp
+        mult = multForEnemyCount enemy.count
+        adjust = floor (mult * toFloat totalXp)
+    in
+        EnemyXpCalc enemy totalXp mult adjust
